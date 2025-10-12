d5ed33931982e09d7a47357072a5f65d59e2dd8e
Leila Chouman <46590542+LeilaChouman@users.noreply.github.com>
2025-10-12 18:16:51 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,9 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBiuX4eQjRXP-kRK0s8w-rxp3v2PJpR6SE",
-  authDomain: "content-partnerships-status.firebaseapp.com",
-  projectId: "content-partnerships-status",
-  storageBucket: "content-partnerships-status.firebasestorage.app",
-  messagingSenderId: "994875499391",
-  appId: "1:994875499391:web:2ace01420b2ca38c9ab04a"
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
